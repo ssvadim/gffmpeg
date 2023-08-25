9c6fc9beb26ba8d1c50b61d01891f95075a43f71
Vadim Storozhev <ssvadim@posteo.net>
2023-08-26 04:22:50 +0500

    bug fix

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-from fastapi import FastAPI, UploadFile, Form, HTTPException
-from starlette.responses import FileResponse
 import subprocess
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
+from starlette.responses import FileResponse
 
 app = FastAPI()
 
 @app.post('/')
-async def convert_audio(file: UploadFile = File(...),
+async def convert(file: UploadFile = File(...),
                         output_format: str = Form('mp3'),
                         params: str = Form('-i')):
     with open('/tmp/input_file', 'wb') as buffer:
         buffer.write(await file.read())
-    ffmpeg_command = ['ffmpeg', '-y'] + params.split(" ") + ['/tmp/input_file', f'/tmp/output.{output_format}']
-    process = subprocess.run(ffmpeg_command)
+    ffmpeg_command = ['ffmpeg', '-y'] + params.split(" ") + \
+                    ['/tmp/input_file', f'/tmp/output.{output_format}']
+    process = subprocess.run(ffmpeg_command, check=True)
     if process.returncode != 0:
         raise HTTPException(status_code=500, detail="FFmpeg failed to convert the file.")
     return FileResponse(f'/tmp/output.{output_format}', filename=f'output.{output_format}')
